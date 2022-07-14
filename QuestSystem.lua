cf7460ccf1fdd146f82b6375496e17a16fcda200
Helicopterboy13 <106978791+Helicopterboy13@users.noreply.github.com>
2022-07-14 01:10:09 +0100

    Add files via upload

--- QuestSystem.lua
+++ QuestSystem.lua
@@ -13,13 +13,81 @@ end
 if CLIENT then
 
     net.Receive("QSMenuCaller",function()
-        QSMenuCall()
+        QSMenuCall("Daily")
         print("Gottim Net Boi")
 
 
     end)
 
-    function QSMenuCall()
+    local QSMenuButtons = function(QSMenuTab, FrameW, FrameH, frame)
+        if QSMenuTab == "Daily" then
+            print("Daily Quests Render")
+
+            local myProgressBar = vgui.Create( "DProgressBar",frame)
+            myProgressBar:SetSize(FrameW/6, FrameH/10)
+            myProgressBar:SetPos( 25, 25 )
+            myProgressBar:SetMin( 0 )
+            myProgressBar:SetMax(16)
+            myProgressBar:SetValue(12)
+            
+
+
+            function CloseWindow()
+                print("Close the Menu")
+                frame:Close()
+            end
+    
+            local CloseButton = vgui.Create("DButton", frame)
+            CloseButton:CenterHorizontal( 0.725 )
+            CloseButton:CenterVertical( 0.89 )
+            CloseButton:SetSize(FrameW/6, FrameH/10)
+            CloseButton:SetText("CLOSE")
+            CloseButton.DoClick = CloseWindow
+            CloseButton.Paint = function(s,w,h)
+                draw.RoundedBox(5,0,0,w,h,Color(255,255,255,100))
+            end
+    
+            function QSReroll()
+                print("Reroll them Quests")
+                -- Deduct Credits
+                -- Rerun the Quest Assaignment algorithm
+            end
+    
+            local RerollButton = vgui.Create("DButton", frame)
+            RerollButton:CenterHorizontal(0.45)
+            RerollButton:CenterVertical(0.89)
+            RerollButton:SetSize(FrameW/6, FrameH/10)
+            RerollButton:SetText("REROLL")
+            RerollButton.DoClick = QSReroll
+            RerollButton.Paint = function(s,w,h)
+                draw.RoundedBox(5,0,0,w,h,Color(255,255,255,100))
+            end
+
+            function QSHelpMenu()
+                print("Open the Help Menu")
+
+            end
+
+            local HelpButton = vgui.Create("DButton", frame)
+            HelpButton:CenterHorizontal(0.175)
+            HelpButton:CenterVertical(0.89)
+            HelpButton:SetSize(FrameW/6, FrameH/10)
+            HelpButton:SetText("HELP")
+            HelpButton.DoClick = QSHelpMenu
+            HelpButton.Paint = function(s,w,h)
+                draw.RoundedBox(5,0,0,w,h,Color(255,255,255,100))
+            end
+
+        elseif QSMenuTab == "Weekly" then
+            print("Weekly Quests Render")
+
+        elseif QSMenuTab == "LifeTime" then
+            print("LifeTime Quests Render")
+        end
+    end
+
+
+    function QSMenuCall(Type)
         -- create the box window and set the settings for it
         local frame = vgui.Create("DFrame")
         local FrameW = ScrW()/2
@@ -33,33 +101,67 @@ if CLIENT then
         frame:SetIsMenu(true) -- Treats this as a menu
 
         frame.Paint = function(s,w,h) -- Overrule frame.paint function
+
+            InnerW = w-FrameW/70
+            InnerH = h-FrameH/40
+
             draw.RoundedBox(5,0,0,w,h,Color(100,50,50,255)) -- Opaque window, grey colour, corner rounding is 5 pixels, Pos set from top right of the 2d frame creation, set to fill the same size of the window
             
-            draw.RoundedBox(5,6,6,w-12,h-12,Color(36,36,36,245)) -- 2nd layer of different colour to create a 4 pixel border on the window, this is main grey, above is border grey
+            draw.RoundedBox(5,FrameW/140,FrameH/80,InnerW,InnerH,Color(36,36,36,245)) -- 2nd layer of different colour to create a 4 pixel border on the window, this is main grey, above is border grey
+
+            draw.RoundedBox(0,FrameW/140+1,FrameH/80,InnerW,InnerH/11,Color(180,10,20,245))
+
+            draw.SimpleText(
+                "Noxifier Quest System",
+                "TitleFontHeli",
+                FrameW*0.225, 
+                FrameH/22, 
+                Color(40,40,150),
+                0,
+                1
+            )
+
+
+
         end
 
-        draw.SimpleText(
-            "Noxifier Quest System",
-            "TitleFontHeli",
-            10 + 10, 
-            20, 
-            Color(150,40,40),
-            0,1
-        )
+        QSMenuButtons("Daily", FrameW, FrameH, frame) -- Call the Buttons, draw em as daily
 
-        function CloseWindow()
-            print("the button has been pressed")
-            frame:Close()
-        end
-
-        local button = vgui.Create("DButton", frame)
-        button:CenterHorizontal( 0.4 )
-        button:CenterVertical( 0.9 )
-        button:SetSize(FrameW/8, FrameH/12)
-        button:SetText("CLOSE")
-        button.DoClick = CloseWindow
-        button.Paint = function(s,w,h)
-            draw.RoundedBox(5,0,0,w,h,Color(255,255,255,100))
-        end
     end
 end
+
+surface.CreateFont( "TitleFontHeli", {
+    font = "Orbitron", --  Use the font-name which is shown to you by your operating system Font Viewer, not the file name
+    extended = false,
+    size = ScrH()/20,
+    weight = 500,
+    blursize = 0,
+    scanlines = 0,
+    antialias = true,
+    underline = false,
+    italic = false,
+    strikeout = false,
+    symbol = false,
+    rotary = false,
+    shadow = false,
+    additive = false,
+    outline = false,
+} )
+
+surface.CreateFont( "contFontHeli", {
+    font = "Orbitron", --  Use the font-name which is shown to you by your operating system Font Viewer, not the file name
+    extended = false,
+    size = ScrH()/50,
+    weight = 500,
+    blursize = 0,
+    scanlines = 0,
+    antialias = true,
+    underline = false,
+    italic = false,
+    strikeout = false,
+    symbol = false,
+    rotary = false,
+    shadow = false,
+    additive = false,
+    outline = false,
+} )
